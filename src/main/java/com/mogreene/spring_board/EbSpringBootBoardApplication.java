c616695bfeca17055938795e721befeb6b06faa9
Mo-Greene <13blueboy13@daum.net>
2023-02-18 19:31:19 +0900

    Initial commit

---
+++ src/main/java/com/mogreene/spring_board/EbSpringBootBoardApplication.java
@@ -0,0 +1,13 @@
+package com.mogreene.spring_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbSpringBootBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbSpringBootBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mogreene/spring_board/EbSpringBootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mogreene.spring_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbSpringBootBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
