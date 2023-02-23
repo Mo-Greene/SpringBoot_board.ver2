fdc05561594dae44c842e52332ed7e47b405065b
Mo-Greene <13blueboy13@daum.net>
2023-02-23 20:30:46 +0900

    주석

--- src/main/java/com/mogreene/spring_board/controller/BoardController.java
+++ src/main/java/com/mogreene/spring_board/controller/BoardController.java
@@ -140,7 +140,7 @@ public class BoardController {
      * @param boardDTO
      * @return
      */
-    // TODO: 2023/02/23 수정버튼 클릭시 조회수 증가 수정필요 
+    // TODO: 2023/02/23 수정버튼 클릭시 조회수 증가 수정필가
     @PostMapping("/modify/{bno}")
     public String updateArticle(BoardDTO boardDTO) {
         log.info("bno : " + boardDTO.getBno());
